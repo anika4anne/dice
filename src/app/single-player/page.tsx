c21e5da4ee4d4e2491efc62c2d921bbbd3711126
Anika Anne <anika4dev@gmail.com>
2025-08-04 15:57:27 -0400

    set up options

---
+++ src/app/single-player/page.tsx
@@ -0,0 +1,8 @@
+export default function MyPage() {
+  return (
+    <main className="flex min-h-screen justify-center bg-gradient-to-b from-purple-900 to-black pt-4 text-white">
+      <h3 className="mb-4 text-3xl font-bold">Single Player Page</h3>
+      <div className="text-center"> </div>
+    </main>
+  );
+}
